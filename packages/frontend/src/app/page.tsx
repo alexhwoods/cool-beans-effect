00a197c1eee7646552e11dd551531b7e3f7a97f6
alexhwoods <alexhwoods@gmail.com>
2025-10-19 15:31:44 -0400

    --wip-- [skip ci]

---
+++ packages/frontend/src/app/page.tsx
@@ -0,0 +1,39 @@
+import { FetchHttpClient } from "@effect/platform";
+import { RpcClient, RpcSerialization } from "@effect/rpc";
+import { Effect, Layer, Stream } from "effect";
+import { UserRpcs } from "@collector/shared";
+import type { User } from "@collector/shared";
+
+import Image from "next/image";
+
+const ProtocolLive = RpcClient.layerProtocolHttp({
+  url: "http://localhost:8000/rpc",
+}).pipe(
+  Layer.provide([
+    // use fetch for http requests
+    FetchHttpClient.layer,
+    // use ndjson for serialization
+    RpcSerialization.layerNdjson,
+  ])
+);
+
+export default async function Home() {
+  // Choose which protocol to use
+
+  function listUsers() {
+    return Effect.gen(function* () {
+      const client = yield* RpcClient.make(UserRpcs);
+      return yield* Stream.runCollect(client.UserList()).pipe(
+        Effect.map((users) => Array.from(users))
+      );
+    }).pipe(
+      Effect.scoped,
+      Effect.provide(ProtocolLive),
+      Effect.runPromise<Array<User>, any>
+    );
+  }
+
+  const users = await listUsers();
+
+  return <div>{JSON.stringify(users)}</div>;
+}
